14e6bbcbfba658af840669fcdf131bd4a6982b7e
Leonardo Correa <leonardoanalista@gmail.com>
2020-02-26 12:42:57 +1100

    npm files to use acual components

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// import SwipeablePanel from './components/Panel';
 import SwipeablePanel from './examples/actualComponents/Panel';
+// import SwipeablePanel from './components/Panel';
 
 export default SwipeablePanel;
